dfd786ad54987fa4372371f53a4cd129315340ad
fullzer4 <gabrielpelizzaro@gmail.com>
2023-03-03 10:06:32 -0300

    new

--- app/src-tauri/src/main.rs
+++ app/src-tauri/src/main.rs
@@ -5,13 +5,13 @@
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn login(email: &str, pass: &str, newpass: &str) -> () {
+    println!("{}", email)
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![login])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- app/src/components/login.tsx
+++ app/src/components/login.tsx
@@ -1,11 +1,19 @@
 import Link from "next/link"
 import Styles from "../scss/login.module.scss"
+import { invoke } from '@tauri-apps/api/tauri'
 
 export default function Login(){
+
+    const Loginauth = () => {
+        invoke.call("login")
+    }
 
     return(
         <div className={Styles.login}>
             <input type="email"/>
+            <input type="password"/>
+            <input type="password"/>
+            <button onClick={() => Loginauth()}>Logar</button>
             <Link href="/signup">Signup</Link>
         </div>
     )
