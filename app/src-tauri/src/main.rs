199255054cf59588e39daf859e30bc43cd57721f
fullzer4 <gabrielpelizzaro@gmail.com>
2023-03-04 01:57:31 -0300

    new

--- app/src-tauri/src/main.rs
+++ app/src-tauri/src/main.rs
@@ -5,7 +5,14 @@
 
 mod auth;
 
+use std::process::Command;
+
 fn main() {
+
+    Command::new("meu_executavel.exe")
+            .spawn()
+            .expect("Falha ao iniciar o executável");
+
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![auth::login])
         .run(tauri::generate_context!())
